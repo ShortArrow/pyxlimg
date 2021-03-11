7c152830cba5d88c964ceb769bed01a37eda8321
ShortArrow <bamboogeneral@live.jp>
2021-03-11 16:00:34 +0900

    fixturize test

--- tests/test_xlimg.py
+++ tests/test_xlimg.py
@@ -3,15 +3,17 @@ import pytest
 from pyxlimg import xlimg
 
 
-def test_openbook():
-    TestBookName = "./tests/testdata/TestBook.xlsx"
-    TargetBook:xlimg.ImageBook = xlimg.ImageBook()
-    TargetBook.open(TestBookName)
+TestBookName = "./tests/testdata/TestBook.xlsx"
+
+@pytest.fixture
+def TargetBook():
+    theBook: xlimg.ImageBook = xlimg.ImageBook()
+    theBook.open(TestBookName)
+    yield theBook
+
+def test_openbook(TargetBook):
     assert TargetBook.name == TestBookName
 
 
-def test_count_sheets():
-    TestBookName = "./tests/testdata/TestBook.xlsx"
-    TargetBook:xlimg.ImageBook = xlimg.ImageBook()
-    TargetBook.open(TestBookName)
+def test_count_sheets(TargetBook):
     assert len(TargetBook.Sheets) == 3
